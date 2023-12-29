8f8fbf6bd6493b4f6408e8cb72af63555b9f8466
ThaisSilva <thaisilva1c2013@email.com>
2023-12-29 10:31:12 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿
+﻿using System.Globalization;
+using System.Runtime;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+      
+Console.WriteLine("Modelo Nokia:");
+Smartphone nokia = new Nokia ("1234", "modelo x", "Imei 11111111", 128);
+nokia.InstalarAplicativo("Instagram");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Modelo Iphone:");
+Smartphone iphone = new Iphone("4321", "modelo w", "Imei 00000000", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
